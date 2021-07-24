14b068204d68683ff290a29631f709ef95ad319a
day qinyanjui <49823575+qinyanjuidavid@users.noreply.github.com>
2021-07-24 16:12:40 +0300

    Created the Chat room

--- chat/views.py
+++ chat/views.py
@@ -8,3 +8,9 @@ def index(request):
 
     }
     return render(request,"chat/index.html",context)
+
+def room(request,room_name):
+    context={
+        'room_name':room_name
+    }
+    return render(request,"chat/room.html",context)
