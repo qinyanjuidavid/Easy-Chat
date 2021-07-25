51695c918fcb6072c4fcdeebed85f58667161319
day qinyanjui <49823575+qinyanjuidavid@users.noreply.github.com>
2021-07-25 23:18:15 +0300

    Everything seems to work well now

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.utils.safestring import mark_safe
+import json
 
 def index(request):
     context = {
@@ -8,9 +11,11 @@ def index(request):
     }
     return render(request, "chat/index.html", context)
 
-
+@login_required
 def room(request, room_name):
     context = {
-        'room_name': room_name
+        'room_name': room_name,
+        'username':mark_safe(json.dumps(request.user.username))
+
     }
     return render(request, "chat/room.html", context)
